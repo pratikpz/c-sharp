dee6b1cf6769f325569fd6fec0dbec92ad09126a
Aakashkandel <Aakashkandel9777@gmail.com~>
2024-01-24 12:39:53 +0545

    this is c-sharp

--- Volume.cs
+++ Volume.cs
@@ -1,7 +1,7 @@
 using System;
 public class VolumeConverter{
     public static void Main(string[] args){
-        Console.WriteLine("Enter the Volume in ml");
+        Console.WriteLine("Enter required  Volume in ml");
         int v = Convert.ToInt32(Console.ReadLine());
         int liter = v /1000;
         int ml = v - (1000*liter);
